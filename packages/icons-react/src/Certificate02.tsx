c1acd9fce60dc03be4f941e4eeb13d85496fd308
Marton Lederer <marton@lederer.hu>
2023-10-11 19:33:32 +0200

    fix: add viewBox

--- packages/icons-react/src/Certificate02.tsx
+++ packages/icons-react/src/Certificate02.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { SVGProps } from 'react';
 const Certificate02 = (props: SVGProps<SVGSVGElement>) => (
   <svg
+    viewBox="0 0 24 24"
     xmlns="http://www.w3.org/2000/svg"
     width={24}
     height={24}
